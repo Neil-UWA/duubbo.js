c634260f56a2d62af73622e143a0bcdb31f463ea
黑卡 <heika@goluckyvip-inc.com>
2023-11-20 11:26:23 +0800

    fix dubboSetting export

--- packages/dubbo-consumer/src/index.ts
+++ packages/dubbo-consumer/src/index.ts
@@ -17,8 +17,8 @@
 
 import java from 'js-to-java'
 import Dubbo from './dubbo'
-import * as s from './dubbo-setting'
+import * as dubboSetting from './dubbo-setting'
 import DubboDirectlyInvoker from './dubbo-directly-invoker'
 import { TDubboCallResult } from './types'
 
-export { java, Dubbo, DubboDirectlyInvoker, s, TDubboCallResult }
+export { java, Dubbo, DubboDirectlyInvoker, dubboSetting, TDubboCallResult }
